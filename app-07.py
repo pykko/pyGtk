4c08b261f4d2f09da4cf018e0951ed800172380b
xilim <slam@xilim/com>
2018-09-17 15:34:24 +0200

    0.0.1

--- app-07.py
+++ app-07.py
@@ -3,34 +3,22 @@
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import GLib , Gio , Gtk
+from gi.repository import Gtk
 
 
-MENU_XML = '''
-<?xml version="1.0" encoding="UTF-8"?>
-<interface>
-	<menu id="app-menu" >
-		<section>
-			<item>
-				<attribute name="action">app.about</attribute>
-				<attribute name="label">_About</attribute>
-			</item>
-			<item>
-				<attribute name="action">app.quit</attribute>
-				<attribute name="label">_Quit</attribute>
-			</item>
-		</section>
-	</menu>
-</interface>
-'''
+app = Gtk.Application()
 
 
 win = Gtk.Window( title = 'Foody' )
 win.set_title( 'Foody' )
 win.connect("destroy", Gtk.main_quit)
 
-builder = Gtk.Builder.new_from_string( MENU_XML , -1 )
-set_app_menu( builder.get_object( 'app-menu' ) )
+builder = Gtk.Builder()
+builder.add_from_file( "menubar-07.ui" )
+win.set_app_menu( builder.get_object( 'menubar' ) )
 
 win.show_all()
 Gtk.main()
+
+
+
